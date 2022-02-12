086e07294837537ec26e21cd18f8dc4656cf69ad
YourContender <dream-boy123@ukr.net>
2022-02-13 01:29:59 +0200

    upgrade2

--- udemy-uber-project/src/js/index.js
+++ udemy-uber-project/src/js/index.js
@@ -1,5 +1,17 @@
-const btn = document.querySelector('.subheader_btn');
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
 
-btn.addEventListener('click', () => {
-    alert('success')
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
 })
